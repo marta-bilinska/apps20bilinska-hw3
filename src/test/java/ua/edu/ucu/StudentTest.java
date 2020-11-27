945c16c0912801486b29dba40022e02edd51aea4
Marta <marta@intteh.com>
2020-11-27 18:55:54 +0200

    some tests added

--- src/test/java/ua/edu/ucu/StudentTest.java
+++ src/test/java/ua/edu/ucu/StudentTest.java
@@ -1,17 +1,30 @@
 package ua.edu.ucu;
 
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+
+import static org.junit.Assert.*;
 
 
 public class StudentTest {
     Student studentOne = new Student("Ivar", "Grimstad", 3.9, 2);
-    Student studentTwo = new Student("Ivar", "Grimstad", 3.9, 2);
+    Student studentTwo = new Student("Ivak", "Grimstad", 3.9, 2);
+    Student studentThree = new Student("Ivar", "Neri", 3.9, 2);
+    Student studentFour = new Student("Ivar", "Grimstad", 3.8, 2);
+    Student studentFive = new Student("Ivar", "Grimstad", 3.9, 3);
+    Student studentSix = new Student("Ivar", "Grimstad", 3.9, 2);
+
+    @Test
+    public void testEquals() {
+        assertNotEquals(null, studentOne);
+        assertNotEquals(studentTwo, studentThree);
+        assertNotEquals(studentFour, studentTwo);
+        assertNotEquals(studentFour, studentFive);
+        assertNotEquals(studentFour, studentTwo);
+    }
 
     @Test
     public void testHash() {
-        assertEquals(studentOne.hashCode(), studentTwo.hashCode());
+        assertEquals(studentOne.hashCode(), studentSix.hashCode());
     }
 
     @Test
@@ -20,22 +33,17 @@ public class StudentTest {
     }
 
     @Test
-    public void testString(){
-        assertEquals(studentOne.toString(),"Student{name=Ivar, surname=Grimstad, GPA=3.9, year=2}");
+    public void testString() {
+        assertEquals(studentOne.toString(), "Student{name=Ivar, surname=Grimstad, GPA=3.9, year=2}");
     }
+
     @Test
-    public void testEqualNull(){
+    public void testEqualNull() {
         assertFalse(studentOne.equals(null));
     }
 
     @Test
-    public void testEquals(){
-
-        assertFalse(studentOne.equals(null));
-    }
-
-    @Test
-    public void testEqualDifferentClass(){
+    public void testEqualDifferentClass() {
         assertFalse(studentOne.equals(new String()));
     }
 
